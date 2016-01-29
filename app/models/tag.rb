aab8cbfb8d29a0cd6f5668ead5d09e77a733432b
eaud <derek.s.mueller@gmail.com>
2016-01-29 14:21:04 -0500

    Cleans out ghost of profile model; limits Users routes

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
-  has_many :profile_tags, dependent: :destroy
-  has_many :profiles, through: :tags
+  has_many :user_tags, dependent: :destroy
+  has_many :users, through: :tags
   has_many :activity_tags, dependent: :destroy
   has_many :activities, through: :activity_tags
   validates_presence_of :name
