33caedd7648dbe90d21a6fd7832392803e2910ce
kaylee42 <kayleeknowles42@gmail.com>
2016-01-29 18:28:42 -0500

    not sure why i need to commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-
-
 tags = ["arts & culture", "books & literature", "cars & motorcycles", "city life", "dancing", "fashion & beauty", "food & drink", "games", "movies & film", "music", "outdoors", "adventure", "hobbies & crafts", "paranormal", "family", "pets & animals", "photography", "sports & recreation", "tech"]
 
 tags.each do |tag|
