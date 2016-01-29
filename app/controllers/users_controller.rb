454d6b1c558333dec72140f958b8d873f52f8f71
jappleba <josh.applebaum@flatironschool.com>
2016-01-29 12:15:39 -0500

    Adds links to user dropdown menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   def show
+    binding.pry
     @user = User.find(params[:id])
-
   end
 
   def edit
+    binding.pry
     @user = current_user
   end
 
   def update
+    binding.pry
     @user = current_user
   end
 
