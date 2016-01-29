dbcdce4aa7c84ed20ee5f2936dd441bf1cfcbaed
kaylee42 <kayleeknowles42@gmail.com>
2016-01-29 09:48:23 -0500

    adds self.find_or_create_from_oath to User, adds create route to sessions controller, adds helper methods current_user and logged_in? to application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,27 @@ class User < ActiveRecord::Base
   has_many :funtimes, through: :activity_guests, source: :activity
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
-  validates_presence_of :uid, :name, :token, :image_url
+  validates_presence_of :uid, :name, :token, :image_url, :token_expiration
   validates_uniqueness_of :uid, :email
 
   def self.new_from_oauth(auth)
     user = User.new
-    user.uid = 
+    user.uid = auth.uid
+    user.name = auth.info.name
+    user.email = auth.info.email
+    user.image_url = auth.info.image
+    user.token = auth.credentials.token
+    user.token_expiration = Time.at(auth.credentials.expires_at)
+    user.save
+    user
+  end
+
+  def self.find_or_create_from_oauth(auth)
+    if user = self.find_by(uid: auth.uid)
+      user.update(token: auth.credentials.token, token_expiration: Time.at(auth.credentials.expires_at))
+    else
+      self.new_from_oauth(auth)
+    end
   end
 
 
