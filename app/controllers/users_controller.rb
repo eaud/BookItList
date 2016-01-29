36e934ea9e62d12f3b02195fa5dcfe595a64eefa
jappleba <josh.applebaum@flatironschool.com>
2016-01-29 13:51:20 -0500

    Fixes update user with private user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,16 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    @user.update(email: params[:user][:email],
-    name: params[:user][:name],
-    bio: params[:user][:bio],
-    image_url: params[:user][:image_url])
+    @user.update(user_params)
     redirect_to user_path
   end
 
   def destroy
     @user = current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :bio, :image_url)
   end
 end
