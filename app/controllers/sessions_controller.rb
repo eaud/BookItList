01614297ecf75a34601be0710fd3134461101e4d
kaylee42 <kayleeknowles42@gmail.com>
2016-01-31 15:53:22 -0500

    still not really working, but several options included in notes file - come back to this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,8 @@ class SessionsController < ApplicationController
     fb_auth_hash = request.env['omniauth.auth']
     @user = User.find_or_create_from_oauth(fb_auth_hash)
     session[:user_id] = @user.id
-    respond_to do |format|
-      format.html{redirect_to root_path}
-      format.js{}
-    end
+    redirect_to root_path
+    
   end
 
   def delete
