0f02567d7b5388de397046a3248e7317065cf843
kaylee42 <kayleeknowles42@gmail.com>
2016-02-02 14:55:57 -0500

    adds chat partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202172206) do
+ActiveRecord::Schema.define(version: 20160202191158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20160202172206) do
     t.integer  "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   add_index "messages", ["chat_id"], name: "index_messages_on_chat_id", using: :btree
