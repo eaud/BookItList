e42d585663ff66310ec2e4e1572bfbeb2eb337e2
kaylee42 <kayleeknowles42@gmail.com>
2016-02-02 12:13:55 -0500

    fixes typo in CreateChats, run migrations

--- db/migrate/20160202165921_create_chats.rb
+++ db/migrate/20160202165921_create_chats.rb
@@ -2,7 +2,7 @@ class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
       t.references :activity, index: true, foreign_key: true
-      t.name :string
+      t.string :name
 
       t.timestamps null: false
     end
