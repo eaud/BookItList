1217c8e19df40143d0c951183ec87ad20119867b
kaylee42 <kayleeknowles42@gmail.com>
2016-02-05 10:30:07 -0500

    fixes error where profpic was deleted from schema

--- db/schema.rb
+++ db/schema.rb
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20160203043606) do
     t.string   "image_url"
     t.string   "token"
     t.datetime "token_expiration"
+    t.string   "profilepic"
     t.json     "score_data"
   end
 
