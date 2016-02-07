0c9bfad433bb86390fc223a2d9c339f091c85f91
kaylee42 <kayleeknowles42@gmail.com>
2016-02-07 13:04:34 -0500

    fixes private messages so that if conversation already exists you are redirected to it

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -8,13 +8,18 @@ class ChatsController < ApplicationController
   end
 
   def new
-    @chat = Chat.new
     @receiver = User.find(request.env["HTTP_REFERER"].split("/")[4])
-    @chat.name = "Private Chat between #{@receiver.name} & #{current_user.name}"
-    @chat.save
-    @chat.users << [@receiver, current_user]
-    @message = Message.new
-    redirect_to chat_path(@chat)
+    if @chat = current_user.chats.where(personal: true).find {|chat|chat.users.include?(@receiver)}
+      redirect_to chat_path(@chat)
+    else
+      @chat = Chat.new
+      @chat.name = "Private Chat between #{@receiver.name} & #{current_user.name}"
+      @chat.personal = true
+      @chat.save
+      @chat.users << [@receiver, current_user]
+      @message = Message.new
+      redirect_to chat_path(@chat)
+    end
   end
 
 
