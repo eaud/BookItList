ec04e83c85aff1ce176d5b41c82e4ff9afe91234
kaylee42 <kayleeknowles42@gmail.com>
2016-02-07 17:25:30 -0500

    fixes activity ordering and automatically appending new activities to your list

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,8 +11,8 @@ class ActivitiesController < ApplicationController
 
   def myindex
     if logged_in?
-      @open_activities = Activity.where(host_id: current_user.id, aasm_state: "open").sort_by do |activity| activty.updated_at end.reverse!
-      @closed_activities = Activity.where(host_id: current_user.id, aasm_state: "closed").sort_by do |activity| activty.updated_at end.reverse!
+      @open_activities = Activity.where(host_id: current_user.id, aasm_state: "open").sort_by do |activity| activity.updated_at end.reverse!
+      @closed_activities = Activity.where(host_id: current_user.id, aasm_state: "closed").sort_by do |activity| activity.updated_at end.reverse!
     else
       redirect_to root_path #EVENTUALLY THIS WILL NEED TO BE JS TO SUGGEST LOGGING IN
     end
@@ -31,11 +31,17 @@ class ActivitiesController < ApplicationController
   end
 
   def create
-    binding.pry
     @activity = Activity.new(activity_params)
     @activity.host = current_user
     if @activity.save
-      redirect_to mylist_path
+      if request.env["HTTP_REFERER"].split("/")[3] == "mylist"
+        respond_to do |format|
+          format.js {}
+          format.html {redirect_to mylist_path}
+        end
+      else
+        redirect_to mylist_path
+      end
     else
       render "new"
     end
