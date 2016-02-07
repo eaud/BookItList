2ea2232033aadb7ba9842317efcfb991521ff78f
kaylee42 <kayleeknowles42@gmail.com>
2016-02-07 16:26:59 -0500

    adds modal for new activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,8 +11,8 @@ class ActivitiesController < ApplicationController
 
   def myindex
     if logged_in?
-      @open_activities = Activity.where(host_id: current_user.id, aasm_state: "open")
-      @closed_activities = Activity.where(host_id: current_user.id, aasm_state: "closed")
+      @open_activities = Activity.where(host_id: current_user.id, aasm_state: "open").sort_by do |activity| activty.updated_at end.reverse!
+      @closed_activities = Activity.where(host_id: current_user.id, aasm_state: "closed").sort_by do |activity| activty.updated_at end.reverse!
     else
       redirect_to root_path #EVENTUALLY THIS WILL NEED TO BE JS TO SUGGEST LOGGING IN
     end
@@ -22,8 +22,8 @@ class ActivitiesController < ApplicationController
     if logged_in?
       @activity = Activity.new
       respond_to do |format|
-        format.html{}
         format.js{}
+        format.html{}
       end
     else
       redirect_to root_path #EVENTUALLY THIS WILL NEED TO BE JS TO SUGGEST LOGGING IN
@@ -31,10 +31,11 @@ class ActivitiesController < ApplicationController
   end
 
   def create
+    binding.pry
     @activity = Activity.new(activity_params)
     @activity.host = current_user
     if @activity.save
-      redirect_to activity_path(@activity)
+      redirect_to mylist_path
     else
       render "new"
     end
