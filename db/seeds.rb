52ee78c403905179d6840bac4c59872c84368385
eaud <derek.s.mueller@gmail.com>
2016-02-07 22:21:00 -0500

    Fixes tag seeding for now, need to create user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
  #   Tag.create(name: Faker::Commerce.department(1))
  # end
 
- alltags = Tag.all
+ # alltags = Tag.all
 
  # 20.times do
  #   user = User.create(token: "FdsafasdfasdfsdfasdF4432432423432432fasdfa", uid: Faker::Number.number(17), name: Faker::Name.name, email: Faker::Internet.email, bio: Faker::Hipster.paragraph, image_url: "josh", token_expiration: Time.now)
@@ -17,16 +17,51 @@
  #   user.tags << alltags.sample(3)
  # end
 
- tags = ["arts & culture", "books & literature", "cars & motorcycles", "city life", "dancing", "fashion & beauty", "food & drink", "games", "movies & film", "music", "outdoors", "adventure", "hobbies & crafts", "paranormal", "family", "pets & animals", "photography", "sports & recreation", "tech"]
+ tags = [
+   "art",
+   "books",
+   "literature",
+   "culture",
+   "cars",
+   "nightlife",
+   "dancing",
+   "fashion",
+   "food",
+   "drinks",
+   "games",
+   "film",
+   "music",
+   "pop music",
+   "house music",
+   "rock music",
+   "R&B",
+   "hip hop",
+   "outdoors",
+   "adventure",
+   "crafts",
+   "hobbies",
+   "paranormal",
+   "family",
+   "animals",
+   "photography",
+   "sports",
+   "recreation",
+   "tech",
+   "comedy",
+   "travel",
+   "health",
+   "science",
+   "lifestyle",
+   "community"]
 
- allusers = User.all
- i = 1
- 50.times do
-   activity = Activity.create(host: User.find(i), name: Faker::Hipster.sentence(5), datetime: Faker::Time.forward(30), cost: Faker::Number.decimal(2), guest_min: 1, guest_max: Faker::Number.between(1, 4), details: Faker::Hipster.sentences(3), image_url: Faker::Placeholdit.image)
-   activity.tags << alltags.sample(2)
-   i < 20 ? i += 1 : i = 1
- end
-# 
-# tags.each do |tag|
-#   Tag.create(name: tag)
-# end
+   tags.each do |tag|
+     Tag.create(name: tag)
+   end
+
+ # allusers = User.all
+ # i = 1
+ # 50.times do
+ #   activity = Activity.create(host: User.find(i), name: Faker::Hipster.sentence(5), datetime: Faker::Time.forward(30), cost: Faker::Number.decimal(2), guest_min: 1, guest_max: Faker::Number.between(1, 4), details: Faker::Hipster.sentences(3), image_url: Faker::Placeholdit.image)
+ #   activity.tags << alltags.sample(2)
+ #   i < 20 ? i += 1 : i = 1
+ # end
